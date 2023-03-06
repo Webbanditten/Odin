924d1f2777faad46cf7dc8bb626656d1288c787e
João <41347100+Sefhriloff@users.noreply.github.com>
2023-03-06 18:12:36 +0000

    Update GETPETSTAT.java
    Dont need to pick pet data from db.

--- Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/rooms/items/GETPETSTAT.java
+++ Kepler-Server/src/main/java/org/alexdev/kepler/messages/incoming/rooms/items/GETPETSTAT.java
@@ -1,36 +1,32 @@
-package org.alexdev.kepler.messages.incoming.rooms.items;
+package org.alexdev.kepler.messages.incoming.rooms.user;
 
-import org.alexdev.kepler.dao.mysql.PetDao;
 import org.alexdev.kepler.game.entity.Entity;
-import org.alexdev.kepler.game.item.Item;
-import org.alexdev.kepler.game.item.base.ItemBehaviour;
+import org.alexdev.kepler.game.entity.EntityType;
 import org.alexdev.kepler.game.pets.Pet;
-import org.alexdev.kepler.game.pets.PetDetails;
 import org.alexdev.kepler.game.player.Player;
-import org.alexdev.kepler.game.room.Room;
-import org.alexdev.kepler.game.room.entities.RoomEntity;
-import org.alexdev.kepler.messages.outgoing.rooms.items.DICE_VALUE;
-import org.alexdev.kepler.messages.outgoing.rooms.items.HANDLE_PETSTAT;
-import org.alexdev.kepler.messages.outgoing.rooms.items.STUFFDATAUPDATE;
+
+import org.alexdev.kepler.messages.outgoing.rooms.user.PETSTAT;
 import org.alexdev.kepler.messages.types.MessageEvent;
 import org.alexdev.kepler.server.netty.streams.NettyRequest;
-import org.apache.commons.lang3.StringUtils;
-
-import static java.lang.Integer.parseInt;
 
 
 public class GETPETSTAT implements MessageEvent {
     @Override
     public void handle(Player player, NettyRequest reader) {
-        int entityId = parseInt(reader.readString().split(Character.toString((char)4))[0]);
-        System.out.println("EntityID: : " + entityId);
-        Entity entity = player.getRoomUser().getRoom().getEntityManager().getByInstanceId(entityId);
-        System.out.println("PETID: " + entity.getDetails().getId());
+        if(player.getRoomUser().getRoom() == null) return;
 
-        PetDetails petDetails = PetDao.getPetDetailsById(entity.getDetails().getId());
-        if (petDetails != null) {
-            Pet pet = new Pet(petDetails);
-            player.send(new HANDLE_PETSTAT(entityId, pet));
-        }
+        String data[] = reader.readString().split(Character.toString((char)4));
+        int id = Integer.parseInt(data[0]);
+
+        Entity entity = player.getRoomUser().getRoom().getEntityManager().getByInstanceId(id);
+
+        if(entity == null) return;
+
+        if(entity.getType() != EntityType.PET) return;
+
+        Pet pet = (Pet)entity;
+
+        player.send(new PETSTAT(id, pet));
     }
+
 }
